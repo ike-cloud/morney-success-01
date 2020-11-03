0284b78eadd7903d5dc012d21f23049c52bd24fe
ike <2740262535@qq.com>
2020-11-03 17:09:01 +0800

    init

---
+++ src/views/Money.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout class-prefix="layout">
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags/>
+  </Layout>
+</template>
+
+<script lang="ts">
+import NumberPad from '@/components/Money/NumberPad.vue';
+import Types from '@/components/Money/Types.vue';
+import Notes from '@/components/Money/Notes.vue';
+import Tags from '@/components/Money/Tags.vue';
+export default {
+  name: 'Money',
+  components: {Tags, Notes, Types, NumberPad},
+};
+</script>
+
+<style lang="scss">
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+</style>
