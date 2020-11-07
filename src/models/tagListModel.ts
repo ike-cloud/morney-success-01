b89e43d94a39a5cd6f70298a31b8c1a1bfc9bb91
ike <2740262535@qq.com>
2020-11-07 22:03:02 +0800

    封装 window.createTag

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -2,18 +2,6 @@ import createId from '@/lib/idCreate';
 
 const LocalStorageKeyName='tagList';
 
-type Tag={
-  id:string,
-  name:string
-}
-type TagListModel={
-  data: Tag[],
-  fetch:()=>Tag[],
-  create:(name:string)=>'success'|'duplicated',
-  save:()=>void,
-  remove:(id:string)=>boolean,
-  update:(id:string,name:string)=>'success'|'not found'|'duplicated';
-}
 const tagListModel:TagListModel={
   data:[],
   fetch(){
--- src/views/custom.d.ts
+++ src/views/custom.d.ts
@@ -21,5 +21,6 @@ type TagListModel={
 }
 
 interface Window{
-  tagList:Tag[]
+  tagList:Tag[];
+  creatTag:(name:string)=>void;
 }
